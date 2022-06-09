7e4f6ff9786eac260eb3b326656f5751994fa315
magus <noah@iamnoah.com>
2022-06-09 00:21:02 -0700

    venusaur test image

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use image::Pixel;
 fn main() {
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
-    let img = image::open("./images/pikachu.png").unwrap();
+    // let img = image::open("./images/pikachu.png").unwrap();
     // let img = image::open("./images/charizard.png").unwrap();
+    let img = image::open("./images/venusaur.png").unwrap();
 
     // The dimensions method returns the images width and height.
     println!("dimensions {:?}", img.dimensions());
