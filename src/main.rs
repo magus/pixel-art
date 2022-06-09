0aa65f2e4095b8229a8f672b818425a6ba3e387b
magus <noah@iamnoah.com>
2022-06-09 00:00:40 -0700

    charizard test image

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     // Use the open function to load an image from a Path.
     // `open` returns a `DynamicImage` on success.
     let img = image::open("./images/pikachu.png").unwrap();
+    // let img = image::open("./images/charizard.png").unwrap();
 
     // The dimensions method returns the images width and height.
     println!("dimensions {:?}", img.dimensions());
